0a2b703a83495e9b8badd65e2d666082f48c1116
dixbycanyonbridge <b.c.dixon@hotmail.com>
2026-04-13 22:05:35 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -11,8 +11,8 @@ st.write(
   """
 )
 
-
-sessuib = cnx.sesion()
+cnx=st.connection("snowflake")
+session = cnx.session()
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
