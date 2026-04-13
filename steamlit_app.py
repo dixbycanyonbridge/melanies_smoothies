f08968ef9f4899898450d7cea62e67bb9bbdaa81
dixbycanyonbridge <b.c.dixon@hotmail.com>
2026-04-13 22:48:29 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages.
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app.
@@ -11,8 +10,6 @@ st.write(
   """
 )
 
-cnx=st.connection("snowflake")
-session = cnx.session()
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
@@ -26,6 +23,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx=st.connection("snowflake")
 session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections = 5)
